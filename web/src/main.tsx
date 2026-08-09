ae266ea81bcf73df3ec83dfc8f716f380bee0841
Rayssa Guimarães <guimaraesr305@gmail.com>
2026-08-09 20:50:32 -0300

    style: add IBM Plex type pairing, a typographic scale, and fix reading width on Reference/About

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/500.css";
+import "@fontsource/ibm-plex-sans/600.css";
+import "@fontsource/ibm-plex-sans/700.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/600.css";
+import "@fontsource/ibm-plex-mono/700.css";
 import "./styles/global.css";
 import "./styles/theme-light.css";
 import "./styles/theme-dark.css";
